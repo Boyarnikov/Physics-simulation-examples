41184b42ee6d4ba892242fc2d872bd9c6e397b3b
Boyar <boiarnikov.ia@phystech.edu>
2024-03-12 12:37:59 +0300

    basic PBD added

---
+++ PBDCloth/main.py
@@ -0,0 +1,10 @@
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+from matplotlib import rc
+rc('animation', html='jshtml')
+
+import matplotlib
+matplotlib.rcParams['animation.embed_limit'] = 20000
