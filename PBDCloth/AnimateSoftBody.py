0792f3d051e64c3db5f3b9c32a07bb0c3b542e47
Boyar <boiarnikov.ia@phystech.edu>
2024-03-26 18:18:46 +0300

    Add breaking animations and examples

--- PBDCloth/AnimateSoftBody.py
+++ PBDCloth/AnimateSoftBody.py
@@ -74,8 +74,8 @@ class AnimateSoftBody:
                     v = c.v_start / 2 + np.linspace(0, 1, 4).reshape((4, 1)) * c.v2.reshape((1, 3))
                     points = v[:, None, :] + u[None, :, :]
                     points.reshape((points.shape[0] * points.shape[1], points.shape[2]))
-                    self.actor_constrains[i] = self.ax.plot_surface(points[:, :, 0], points[:, :, 1], points[:, :, 2], alpha=0.1)
-
+                    self.actor_constrains[i] = self.ax.plot_surface(points[:, :, 0], points[:, :, 1], points[:, :, 2],
+                                                                    alpha=0.1)
 
     def _draw_connections(self):
         _left_connections = np.take(self.soft_body.point_position, (self.soft_body.connection[:, 0] * 3,
@@ -97,8 +97,13 @@ class AnimateSoftBody:
                     actor.set_data([0], [0])
                     actor.set_3d_properties(0)
                     continue
+
                 p1 = self.soft_body.point_position[self.soft_body.connection[i, 0]]
                 p2 = self.soft_body.point_position[self.soft_body.connection[i, 1]]
+                if np.linalg.norm(p1 - p2) > self.soft_body.connection_idle_len[i] * 1.1:
+                    actor.set(color="r")
+                else:
+                    actor.set(color="b")
 
                 actor.set_data(np.column_stack((p1, p2))[:2])
                 actor.set_3d_properties(np.column_stack((p1, p2))[2], "z")
--- PBDCloth/main.py
+++ PBDCloth/main.py
@@ -1,3 +1,4 @@
+import math
 import time
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,7 +7,7 @@ import time
 
 from SoftBody import SoftBody
 from AnimateSoftBody import AnimateSoftBody
-from ClothShapes import create_rect_with_strong_edges
+from ClothShapes import create_rect_with_strong_edges, create_rect_x
 from BodyShapes import create_cube_rigid
 from Constrain import PlaneConstrain
 
@@ -20,23 +21,29 @@ matplotlib.rcParams['animation.embed_limit'] = 20000
 DT = 1 / 60
 
 h, w = 10, 10
-points, connections = create_rect_with_strong_edges(h, w,
-                                    starting_point=np.array([0.1, 0.1, 1.0]),
-                                    h_vec=np.array([0.8, 0.0, 0.0]),
-                                    w_vec=np.array([0.0, 0.8, 0.0]))
+points, connections = create_rect_x(h, w,
+                                    starting_point=np.array([0.1, 0.5, 1.0]),
+                                    h_vec=np.array([0.8, 0.001, 0.0]),
+                                    w_vec=np.array([0.0, 0.0, -0.8]))
 
-i = 5
+i = 1
 c = 0.01
 
-sb = SoftBody(_points=points, _connections=connections, iters=i, m=0.5, c=c, b=2)
+sb = SoftBody(_points=points, _connections=connections, iters=i, m=0.1, c=c, b=2)
 sb.fix_points([0])
-sb.fix_points([1])
 sb.fix_points([h - 1])
+
+t = 0
 
 
 def pass_func():
-    sb.point_position[0][1] += 0.003
-    sb.point_position[1][1] += 0.003
+    global t
+    t += 0.02
+    sb.point_position[h - 1][2] -= math.cos(t) * 0.01
+    sb.point_position[h - 1][0] -= math.cos(t) * 0.01
+
+    sb.point_position[0][2] -= math.cos(t) * 0.01
+    sb.point_position[0][1] -= math.cos(t) * 0.02
 
 
 '''
@@ -52,26 +59,31 @@ c = 0.04
 sb = SoftBody(_points=points, _connections=connections, iters=i, m=0.01, c=c)
 
 sb.point_velocity[0] = np.array([80, 30, 50])
+'''
 
 floor = PlaneConstrain(np.zeros(3), np.array([1.0, 0.0, 0.0]), np.array([0.0, 1.0, 0.0]))
 sb.add_constrain(floor)
+
 wall = PlaneConstrain(np.zeros(3), np.array([0.0, 1.0, 0.0]), np.array([0.0, 0.0, 1.0]))
 sb.add_constrain(wall)
+
 wall = PlaneConstrain(np.zeros(3), np.array([0.0, 0.0, 1.0]), np.array([1.0, 0.0, 0.0]))
 sb.add_constrain(wall)
+
 wall = PlaneConstrain(np.array([0.0, 1.0, 0.0]), np.array([1.0, 0.0, 0.0]), np.array([0.0, 0.0, 1.0]))
 sb.add_constrain(wall)
+
 wall = PlaneConstrain(np.array([1.0, 0.0, 0.0]), np.array([0.0, 0.0, 1.0]), np.array([0.0, 1.0, 0.0]))
 sb.add_constrain(wall)
-'''
-frames = 200
+
+frames = 400
 asb = AnimateSoftBody(soft_body=sb, dt=DT, frames=frames, pass_func=pass_func)
 
 ani = animation.FuncAnimation(
     asb.fig, asb.animation_func, frames, fargs=(asb,),
     interval=10)
 
-# writer = animation.PillowWriter(fps=30, bitrate=1800)
-# ani.save(f'bin/Cube_with_physics.gif', writer=writer)
+writer = animation.PillowWriter(fps=30, bitrate=1800)
+ani.save(f'bin/Cloth_with_physics_in_a_box.gif', writer=writer)
 
 plt.show()
